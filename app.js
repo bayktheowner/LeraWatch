67c7e629bbc120f6448347c27dc503fe4f66e9f0
bayktheowner <alxeyonway@gmail.com>
2026-09-25 14:53:24 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -287,7 +287,7 @@ function renderWatchProviders(content, data) {
         return `
           <a class="provider provider-link" href="${escapeAttr(provider.url)}" target="_blank" rel="noopener noreferrer" aria-label="Открыть ${providerName}">
             ${logo}
-            <span>${providerName} ↗</span>
+            <span class="provider-name-clean">${providerName}</span>
           </a>`;
       }
 
